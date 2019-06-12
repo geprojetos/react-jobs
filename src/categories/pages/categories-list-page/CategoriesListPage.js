cc32c4c115cd4b840c1d01ff54b5c98786140190
geprojetos <geprojetosbrasil@gmail.com>
2019-06-12 17:09:12 -0300

    Criando o componente de categorias

--- src/categories/pages/categories-list-page/CategoriesListPage.js
+++ src/categories/pages/categories-list-page/CategoriesListPage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import ListGroup from '../../components/list/ListGroup';
+import ApiService from '../../../shared/services/api/ApiService';
+const api = new ApiService();
+
 class CategoriesListPage extends Component {
     
+    state =  {
+        categories: []
+    };
+
+    async componentDidMount() {
+
+        const categories = await api.get('jobs');
+        
+        this.setState({ categories });
+    };
+
     render() { 
+        const { categories } = this.state;
+
         return (  
-            <h1>Categories</h1>
+            <ListGroup categories={ categories } />
         );
     }
 }
